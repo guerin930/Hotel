4c5e5cd1c963cab1e7b72a5b2c0d1b5e0d0fbc88
guerin930 <armelguerin23@gmail.com>
2022-01-11 17:04:10 +0100

    deploiement
    Signed-off-by: guerin930 <armelguerin23@gmail.com>

---
+++ build/precache-manifest.483ae8fe2fe3fd3cfc3f5c20a0ccb763.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2e6a5ba99c43d7fe1d08d7d8993bfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96becb7fd1aeb5b87072",
+    "url": "/static/css/2.fa1b0089.chunk.css"
+  },
+  {
+    "revision": "e46d7e0516d2578b7f5a",
+    "url": "/static/css/main.b5c59652.chunk.css"
+  },
+  {
+    "revision": "96becb7fd1aeb5b87072",
+    "url": "/static/js/2.1cdc269c.chunk.js"
+  },
+  {
+    "revision": "3f3906ab99d403b8349fadd755429c9d",
+    "url": "/static/js/2.1cdc269c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46d7e0516d2578b7f5a",
+    "url": "/static/js/main.31269672.chunk.js"
+  },
+  {
+    "revision": "9d96126a8f18c0ebbaa0",
+    "url": "/static/js/runtime-main.20a4d22a.js"
+  },
+  {
+    "revision": "991f17a1a00da73b74c137172d322e66",
+    "url": "/static/media/2.991f17a1.jpg"
+  },
+  {
+    "revision": "910d720f11353aee85aabdc291f72449",
+    "url": "/static/media/3.910d720f.jpg"
+  },
+  {
+    "revision": "d1c7e611119fa23c449b0672df09942f",
+    "url": "/static/media/4.d1c7e611.jpg"
+  },
+  {
+    "revision": "aaccb0f30a473b1962fbdf2b5a369226",
+    "url": "/static/media/5.aaccb0f3.jpg"
+  },
+  {
+    "revision": "09b5814c5d43fba28a1e5b4407ef573f",
+    "url": "/static/media/Eru.09b5814c.jpg"
+  },
+  {
+    "revision": "9ac2a60828c8f6e627f4f628899398f0",
+    "url": "/static/media/JosefinSans-Bold.9ac2a608.ttf"
+  },
+  {
+    "revision": "b416cdfefb8773398d7bde7095d7b48d",
+    "url": "/static/media/JosefinSans-ExtraLight.b416cdfe.ttf"
+  },
+  {
+    "revision": "6762afeccd02d16a80409b78fb85251c",
+    "url": "/static/media/JosefinSans-Regular.6762afec.ttf"
+  },
+  {
+    "revision": "c009f141a4b804814d7a26d81d7d6488",
+    "url": "/static/media/Ndolé.c009f141.jpg"
+  },
+  {
+    "revision": "530e1e4291d2a3101bab475562a695a2",
+    "url": "/static/media/agree.530e1e42.svg"
+  },
+  {
+    "revision": "0272f3a7c8b7321716f0783d4ae6372c",
+    "url": "/static/media/angle.0272f3a7.PNG"
+  },
+  {
+    "revision": "eb56b4513267afdf7cb9be42743296a0",
+    "url": "/static/media/bed1.eb56b451.jpg"
+  },
+  {
+    "revision": "89345806d561b5f9fcea954546f76bac",
+    "url": "/static/media/couloir.89345806.PNG"
+  },
+  {
+    "revision": "4072dfe37b2a8ba5e0b421c8982c4f30",
+    "url": "/static/media/defaultBcg.4072dfe3.jpg"
+  },
+  {
+    "revision": "33ab9f1e88f204b5b8a13725a918f36c",
+    "url": "/static/media/details-1.33ab9f1e.jpeg"
+  },
+  {
+    "revision": "6edb36f3feccea6f8483d2808828b6e1",
+    "url": "/static/media/error.6edb36f3.jpg"
+  },
+  {
+    "revision": "77dec4336b21538ffabc1fbf5194362a",
+    "url": "/static/media/grillade.77dec433.jpg"
+  },
+  {
+    "revision": "44a1733c1b5dd262d15a8dec011b8df4",
+    "url": "/static/media/homard.44a1733c.jpg"
+  },
+  {
+    "revision": "ce6b795cf63d1c7517e741abd21c169b",
+    "url": "/static/media/icon.ce6b795c.svg"
+  },
+  {
+    "revision": "88c9c1839a82ab6854f53a208295e354",
+    "url": "/static/media/icon2.88c9c183.svg"
+  },
+  {
+    "revision": "dac89139e502ea99fae573f857c10f9f",
+    "url": "/static/media/icon3s.dac89139.svg"
+  },
+  {
+    "revision": "603d80c0ec9b17a2d2dc4825b17c2e2e",
+    "url": "/static/media/icon4.603d80c0.svg"
+  },
+  {
+    "revision": "f981aa7a1799f2215f46d530a1a9dbb1",
+    "url": "/static/media/j2.f981aa7a.PNG"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "d3590bf28d50e01443fc75ea95363b5a",
+    "url": "/static/media/login.d3590bf2.svg"
+  },
+  {
+    "revision": "2252194ab79545d41c8bcba5a33bd2b9",
+    "url": "/static/media/logout.2252194a.svg"
+  },
+  {
+    "revision": "2bc434f526b458bd93d5c899372b60f7",
+    "url": "/static/media/lomo_saltado.2bc434f5.jpg"
+  },
+  {
+    "revision": "8c3d2a79d2974762d90310ec87bec6e6",
+    "url": "/static/media/notfound.8c3d2a79.svg"
+  },
+  {
+    "revision": "ccad5f017c9f27c3472f658619281f2c",
+    "url": "/static/media/pizza-3.ccad5f01.jpg"
+  },
+  {
+    "revision": "b8afd867a9a461375909b24b11efa6b3",
+    "url": "/static/media/pizza.b8afd867.jpg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "3b5c08716be6b713154c8aedaec3f5bf",
+    "url": "/static/media/room-10.3b5c0871.jpeg"
+  },
+  {
+    "revision": "f08fee37bdc8f3d11ef36658cfacc44e",
+    "url": "/static/media/room-11.f08fee37.jpeg"
+  },
+  {
+    "revision": "0e293352532d9483616fe4c5110c8e28",
+    "url": "/static/media/room-12.0e293352.jpeg"
+  },
+  {
+    "revision": "11343adfcff21648fa4c3128e13f88aa",
+    "url": "/static/media/room-2.11343adf.jpg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/static/media/room-3.9a1103b5.jpeg"
+  },
+  {
+    "revision": "b9a34e969ab86c1500d67183f646995a",
+    "url": "/static/media/room-6.b9a34e96.jpeg"
+  },
+  {
+    "revision": "e251c8d571a7f753b3da985e4b8a34c6",
+    "url": "/static/media/room-7.e251c8d5.jpeg"
+  },
+  {
+    "revision": "ebb7fca5f456fb07e0096ab3ef72b330",
+    "url": "/static/media/room-8.ebb7fca5.jpeg"
+  },
+  {
+    "revision": "7b17c49f01aa17fc11df8400d604e1c9",
+    "url": "/static/media/room-9.7b17c49f.jpeg"
+  },
+  {
+    "revision": "4c8a88b538e4331d21f8a459820fbdf5",
+    "url": "/static/media/room.4c8a88b5.jpg"
+  },
+  {
+    "revision": "9e1449cb16eedcdb45ebddbf6bcfbcc7",
+    "url": "/static/media/s2.9e1449cb.PNG"
+  },
+  {
+    "revision": "a81dcc2e2bc490dd55aa8d9f78ddedf9",
+    "url": "/static/media/s3.a81dcc2e.PNG"
+  },
+  {
+    "revision": "8ab2aa964d478610f06524a09f5ef58d",
+    "url": "/static/media/saka_saka.8ab2aa96.jpg"
+  },
+  {
+    "revision": "60d4792e12e3600f28201b10ab8700e6",
+    "url": "/static/media/screen 2.60d4792e.PNG"
+  },
+  {
+    "revision": "9c6337d7fea88650d133d51bc0877b4d",
+    "url": "/static/media/shawamar.9c6337d7.jpg"
+  },
+  {
+    "revision": "99aa72fc57f557b0b78b1b60f28d7b05",
+    "url": "/static/media/ss1.99aa72fc.PNG"
+  },
+  {
+    "revision": "f0e335e62b42162094f0653de7caf9dc",
+    "url": "/static/media/ss2.f0e335e6.PNG"
+  },
+  {
+    "revision": "d9e3de877d6e5d4217a11ff71ea94337",
+    "url": "/static/media/suite j1.d9e3de87.PNG"
+  },
+  {
+    "revision": "a00c7fd78cd758a9089c1de7b9e2f45e",
+    "url": "/static/media/svg-2.a00c7fd7.svg"
+  },
+  {
+    "revision": "7517170f3a331070e2ea3e772074ab9f",
+    "url": "/static/media/svg-4.7517170f.svg"
+  },
+  {
+    "revision": "db52febcef417b93e9280e2f3228c54c",
+    "url": "/static/media/table.db52febc.jpg"
+  },
+  {
+    "revision": "0454905ea9262e30a423fd61c8294347",
+    "url": "/static/media/toilette.0454905e.PNG"
+  },
+  {
+    "revision": "7011287da1dbcd3a102394aac912585c",
+    "url": "/static/media/transfer.7011287d.svg"
+  },
+  {
+    "revision": "87786e75efade5a1aa6a5b2bb11fdeb9",
+    "url": "/static/media/villa.87786e75.jpg"
+  }
+]);
